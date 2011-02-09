c8bdf0d8d7f58ac215a5e71d84d67390bc15d8dc
Johnothan King <johnothanking@protonmail.com>
2011-02-09 05:49:14 -0500

    Version: 2011-02-08 93u
    Sourced from ast-open-history version 2011-02-02b and:
    d692d476fe4bb933e7b6459c0cc99a2e9f428bfd81a495ed4201ec197e070f57  INIT.2011-02-08.tgz
    44ae3913e6c0c8992c6c3cba16864fd4c4a18c6da8e7d87c85a23060cea7cd1a  ast-base.2011-02-08.tgz
    581e317d36ef55505329b5b67b6bc2208b496638a466cad18c6674b60449b01d  ast-ksh.2011-02-08.tgz

--- src/cmd/codexlib/compress/compress.c
+++ src/cmd/codexlib/compress/compress.c
@@ -1,22 +1,70 @@
 /***********************************************************************
 *                                                                      *
-*               This software is part of the ast package               *
-*          Copyright (c) 1986-2011 AT&T Intellectual Property          *
-*                      and is licensed under the                       *
-*                  Common Public License, Version 1.0                  *
-*                    by AT&T Intellectual Property                     *
+*               This software is part of the bsd package               *
+* Copyright (c) 1985-2011 The Regents of the University of California  *
 *                                                                      *
-*                A copy of the License is available at                 *
-*            http://www.opensource.org/licenses/cpl1.0.txt             *
-*         (with md5 checksum 059e8cd6165cb4c31e351f2b69388fd9)         *
+* Redistribution and use in source and binary forms, with or           *
+* without modification, are permitted provided that the following      *
+* conditions are met:                                                  *
 *                                                                      *
-*              Information and Software Systems Research               *
-*                            AT&T Research                             *
-*                           Florham Park NJ                            *
+*    1. Redistributions of source code must retain the above           *
+*       copyright notice, this list of conditions and the              *
+*       following disclaimer.                                          *
 *                                                                      *
-*                 Glenn Fowler <gsf@research.att.com>                  *
-*                  David Korn <dgk@research.att.com>                   *
-*                   Phong Vo <kpv@research.att.com>                    *
+*    2. Redistributions in binary form must reproduce the above        *
+*       copyright notice, this list of conditions and the              *
+*       following disclaimer in the documentation and/or other         *
+*       materials provided with the distribution.                      *
+*                                                                      *
+*    3. Neither the name of The Regents of the University of California*
+*       names of its contributors may be used to endorse or            *
+*       promote products derived from this software without            *
+*       specific prior written permission.                             *
+*                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND               *
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,          *
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF             *
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE             *
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS    *
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,             *
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED      *
+* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,        *
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON    *
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,      *
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY       *
+* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE              *
+* POSSIBILITY OF SUCH DAMAGE.                                          *
+*                                                                      *
+* Redistribution and use in source and binary forms, with or without   *
+* modification, are permitted provided that the following conditions   *
+* are met:                                                             *
+* 1. Redistributions of source code must retain the above copyright    *
+*    notice, this list of conditions and the following disclaimer.     *
+* 2. Redistributions in binary form must reproduce the above copyright *
+*    notice, this list of conditions and the following disclaimer in   *
+*    the documentation and/or other materials provided with the        *
+*    distribution.                                                     *
+* 3. Neither the name of the University nor the names of its           *
+*    contributors may be used to endorse or promote products derived   *
+*    from this software without specific prior written permission.     *
+*                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS"    *
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED    *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A      *
+* PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS    *
+* OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,      *
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT     *
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF     *
+* USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  *
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   *
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT   *
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF   *
+* SUCH DAMAGE.                                                         *
+*                                                                      *
+*                          Diomidis Spinellis                          *
+*                            James A. Woods                            *
+*                            Spencer Thomas                            *
+*                             Joseph Orost                             *
 *                                                                      *
 ***********************************************************************/
 #pragma prototyped
--- src/lib/libz/sfdcgzip.h
+++ src/lib/libz/sfdcgzip.h
@@ -1,7 +1,7 @@
 /***********************************************************************
 *                                                                      *
 *               This software is part of the ast package               *
-*                  Copyright (c) 1995-2005 AT&T Corp.                  *
+*          Copyright (c) 1995-2011 AT&T Intellectual Property          *
 *                                                                      *
 * This software is provided 'as-is', without any express or implied    *
 * warranty. In no event will the authors be held liable for any        *
